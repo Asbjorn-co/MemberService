17c4c27deea4edaa5f734b459248a0c84f1082f1
FynboNohr69420 <117977003+FynboNohr69420@users.noreply.github.com>
2024-03-11 10:47:32 +0100

    MOdul 8

--- MemberService/Controllers/MemberController.cs
+++ MemberService/Controllers/MemberController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MemberService.Models;
+using System.Diagnostics;
 using System.Linq;
 
 namespace MemberService.Controllers;
@@ -57,4 +58,28 @@ public class MemberController : ControllerBase
     {
     return _members.Where(c => c.Id == customerId).First();
     }
-}
+
+    [HttpGet("version")]
+    public async Task<Dictionary<string, string>> GetVersion()
+    {
+        var properties = new Dictionary<string, string>();
+        var assembly = typeof(Program).Assembly;
+        properties.Add("service", "qgt-customer-service");
+        var ver = FileVersionInfo.GetVersionInfo(typeof(Program)
+        .Assembly.Location).ProductVersion;
+        properties.Add("version", ver!);
+        try
+        {
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
+            var ipa = ips.First().MapToIPv4().ToString();
+            properties.Add("hosted-at-address", ipa);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            properties.Add("hosted-at-address", "Could not resolve IP-address");
+        }
+
+        return properties;
+    }
